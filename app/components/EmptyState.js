21e0129d5bc33807b6595f39588e029c2450e4d9
weathery <weathery@qq.com>
2023-09-04 16:18:15 +0800

    feathure: redefine content

--- app/components/EmptyState.js
+++ app/components/EmptyState.js
@@ -1,7 +1,7 @@
 export default function EmptyState({ setOpen, setPrompt }) {
   return (
     <div className="mt-12 sm:mt-24 space-y-6 text-gray-400 text-base mx-8 sm:mx-4 sm:text-2xl leading-12">
-      <p>Send me a message.</p>
+      <p>🦙 What can I do for you ?</p>
     </div>
   );
 }
